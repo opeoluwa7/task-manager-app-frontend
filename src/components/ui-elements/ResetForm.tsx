3a5a5bda4d62b9359ef08068826cbaeedfeac867
Opeoluwa7 <hazeofficial298@gmail.com>
2025-04-23 18:42:47 +0100

    test

--- src/components/ui-elements/ResetForm.tsx
+++ src/components/ui-elements/ResetForm.tsx
@@ -17,7 +17,7 @@ const ResetForm = () => {
 
 
     const resetPassword = async (userData: FormFields) => {
-        const request = await api.post(`${baseUrl}/reset-password`, userData).then((res) => console.log(res.data))
+        const request = await api.post(`${baseUrl}/api/reset-password`, userData).then((res) => console.log(res.data))
 
         return request
     }
