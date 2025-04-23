f9d078e0593d802549d617fb1921cf8bc481cb9c
Opeoluwa7 <hazeofficial298@gmail.com>
2025-04-23 18:24:40 +0100

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import AppRoutes from './routes/AppRoutes.tsx'
-import { BrowserRouter } from 'react-router-dom';
 
 const queryClient = new QueryClient();
 
 function App() { 
 
   return (
-    <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <AppRoutes />
-      </QueryClientProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <AppRoutes />
+    </QueryClientProvider>
+
   )
 }
 
